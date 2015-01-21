0e20fd0ad3232fbd9815e3e708e81fc4564c857f
HerrAugust <HerrAugust@users.noreply.github.com>
2015-01-21 17:28:03 +0100

    Update Main.java

--- MelideoEx/TreeSet_Map/Main.java
+++ MelideoEx/TreeSet_Map/Main.java
@@ -6,14 +6,12 @@ public class Main {
 	}
 	
 	public static void run() {
-		new Ex1().run();
-<<<<<<< HEAD:MelideoEx/TreeSet_Map/Main.java
-		System.out.println("\nBy division:");
-=======
-		System.out.println("\nOrdinamento per divisione:");
->>>>>>> 004d95e6e1e42d13b3a3a7ba65769221666db007:MelideoEx/TreeSet_Map/src/Main.java
+		System.out.println("\nImplementazione con TreeSet:");
+		new Ex1TreeSet().run();
+		System.out.println("\n\nImplementazione con TreeMap:");
+		new Ex1TreeSet().run();
+		System.out.println("\n\nOrdinamento per divisione:");
 		new Ex2().run();
-		
 	}
 
 }
