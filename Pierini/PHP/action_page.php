ee57bb3a6512ad3808b542705e8c4f98e2bdc095
HerrAugust <HerrAugust@users.noreply.github.com>
2015-09-09 17:20:08 +0200

    Create action_page.php

---
+++ Pierini/PHP/action_page.php
@@ -0,0 +1,26 @@
+<?php
+include 'utility.php'; #creaDB()
+
+#connessione/creazione db
+if($_POST['creaDB']) {
+	$link = mysql_connect('localhost', 'root', 'MySQL', '8080');
+	if (!$link) {
+		if(creaDB())
+			die('Ci sono dei problemi con la creazione del Database' . mysql_errno());
+	}
+	echo 'Connected successfully';
+}
+else #esegui query
+{
+	$pieces = explode(' ', $_POST['query']);
+	if($pieces == false or strcmp($pieces[0], "insert") !== 0 or strcmp($pieces[1], "into") !== 0)  {
+		die('La query richiesta non è corretta. Vengono accettate solo quelle di inserimento!');
+	}
+	else {
+		$res = mysql_query($_POST['query']);
+		if(!$res)
+			die("Errore nell'esecuzione della query: " . mysql_errno());
+	}
+}
+
+?>
