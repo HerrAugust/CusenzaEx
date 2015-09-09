2b82b08d23bd0f25bbb770bee1d8b7ae89f61994
HerrAugust <HerrAugust@users.noreply.github.com>
2015-09-09 21:24:04 +0200

    Update action_page.php

--- Pierini/PHP/action_page.php
+++ Pierini/PHP/action_page.php
@@ -1,31 +1,38 @@
 <?php
 include 'utility.php'; #creaDB()
 
+$link = false;
+
 #connessione/creazione db
-if($_POST['creaDB']) {
-	$link = mysql_connect('localhost', 'root', 'MySQL', '8080');
-	if (!$link) {
-		echo 'Connected unsuccessfully';
-	}
-	else { #connessione ok
-		if(databasenotexists() && creaDB($link))
+if(isset($_POST['creaDB'])) {
+	$link = mysql_connect('localhost', 'root', 'MySQL');
+	if($link == false)
+		die('Ci sono problemi di collegamento al DBMS.');
+
+	#se connessione ok
+	if(databaseexists())
+		die("Il database esiste già.");
+	else
+		if(creaDB($link))
 			echo "Database creato correttamente";
 		else
-			die('Ci sono dei problemi con la creazione del Database' . mysql_errno());
-	}
-		
+			echo "Ci sono dei problemi con la creazione del Database. Errore: " . mysql_errno() . "</br>";		
 }
 else #esegui query
 {
-	$pieces = explode(' ', $_POST['query']);
-	if($pieces == false or strcmp($pieces[0], "insert") !== 0 or strcmp($pieces[1], "into") !== 0)  {
-		die('La query richiesta non è corretta. Vengono accettate solo quelle di inserimento!');
-	}
-	else {
-		$res = mysql_query($_POST['query']);
-		if(!$res)
-			die("Errore nell'esecuzione della query: " . mysql_errno());
-	}
+	$link = mysql_connect('localhost', 'root', 'MySQL', 'CentroElaborazione');
+	if($link == false)
+		die('Ci sono problemi di collegamento al DBMS.');
+	echo "La tua query:<b> ".$_POST['query']."</b></br></br>";
+	if(strpos($_POST['query'], "create") !== FALSE)
+		die("Vengono accettate solo query di inserimento!");
+	
+	$res = mysql_query($_POST['query'], $link);
+	if(!$res)
+		die ("Errore nell'esecuzione della query. Errore: " . mysql_errno());
+	else
+		echo "Ho eseguito la query";
 }
 
+mysql_close();
 ?>
