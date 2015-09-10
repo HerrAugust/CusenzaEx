5408602c7d0885257408271754a4895ef73c0c5e
HerrAugust <HerrAugust@users.noreply.github.com>
2015-09-10 16:30:01 +0200

    Update action_page.php

--- Pierini/PHP/action_page.php
+++ Pierini/PHP/action_page.php
@@ -1,14 +1,12 @@
 <?php
 include 'utility.php'; #creaDB()
 
-$link = false;
-
-#connessione/creazione db
-if(isset($_POST['creaDB'])) {
-	$link = mysql_connect('localhost', 'root', 'MySQL');
+$link = mysql_connect('localhost', 'root', 'MySQL');
 	if($link == false)
 		die('Ci sono problemi di collegamento al DBMS.');
 
+#connessione/creazione db
+if(isset($_POST['creaDB'])) {
 	#se connessione ok
 	if(databaseexists())
 		die("Il database esiste già.");
@@ -20,16 +18,17 @@ if(isset($_POST['creaDB'])) {
 }
 else #esegui query
 {
-	$link = mysql_connect('localhost', 'root', 'MySQL', 'CentroElaborazione');
-	if($link == false)
-		die('Ci sono problemi di collegamento al DBMS.');
+	if(!mysql_select_db('CentroElaborazione'))
+		die("Impossibile connettersi al DB CentroElaborazione. Errore: " . mysql_error());
+
+	
 	echo "La tua query:<b> ".$_POST['query']."</b></br></br>";
 	if(strpos($_POST['query'], "create") !== FALSE)
 		die("Vengono accettate solo query di inserimento!");
 	
 	$res = mysql_query($_POST['query'], $link);
 	if(!$res)
-		die ("Errore nell'esecuzione della query. Errore: " . mysql_errno());
+		die ("Errore nell'esecuzione della query. Errore: " . mysql_error());
 	else
 		echo "Ho eseguito la query";
 }
