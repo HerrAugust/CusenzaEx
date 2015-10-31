dd59696c88815121119196d75619873899878fae
HerrAugust <HerrAugust@users.noreply.github.com>
2015-10-31 19:40:10 +0100

    Create client.h

---
+++ Embedded-Systems/client.h
@@ -0,0 +1,13 @@
+/*
+ * client.h
+ *
+ *  Created on: 24/ott/2015
+ *      Author: Agostino
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+bool sendToComputer(const char*);
+
+#endif /* CLIENT_H_ */
