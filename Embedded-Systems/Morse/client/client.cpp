fd4bb374ba429fba1bad91c38bd471af8914aca3
HerrAugust <HerrAugust@users.noreply.github.com>
2015-11-01 21:04:15 +0100

    Update and rename Embedded-Systems/Morse/client.cpp to Embedded-Systems/Morse/client/client.cpp

--- Embedded-Systems/Morse/client/client.cpp
+++ Embedded-Systems/Morse/client/client.cpp
@@ -1,9 +1,44 @@
 /*
- * client.cpp
+ * Author: Agostino Mascitti <agostino690@gmail.com>
+ * Copyright (c) 2015 Intel Corporation.
  *
- *  Created on: 24/ott/2015
- *      Author: Agostino Mascitti
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+/**
+ * @file
+ * @ingroup grove
+ * @brief Morse interpreter using Internet example
+ *
+ * This project shows how to use Grove Touch or Button sensor to read a string
+ * and than pass it to your computer through Internet.
+ *
+ * @hardware Sensors used:
+ * Grove Touch (GroveTouch)
+ * Grove Button (GroveButton)
+ *
+ * @ld -lupm-grove
+ *
+ * @date 24/ott/2015
+ */
+
 #include "./client.h"
 
 #include <iostream>
@@ -25,6 +60,7 @@ using namespace std;
  * This function sends your Morse string (inserted by using the Grove Touch or Button sensor) to
  * your computer. This, in turn, will open a browser to pronounce your Morse string.
  * This function is IP version independent, ie you can use either IPv4 or IPv6.
+ * You must start ServerIntelEdison.java on your computer to make it work.
  */
 bool sendToComputer(const char* message) {
 #ifdef DEBUG
