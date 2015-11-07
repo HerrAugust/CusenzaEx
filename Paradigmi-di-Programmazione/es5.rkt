99969b8944aed7a7e98e92df87e917714d866c3c
HerrAugust <HerrAugust@users.noreply.github.com>
2015-11-07 21:37:17 +0100

    Update es5.rkt

--- Paradigmi-di-Programmazione/es5.rkt
+++ Paradigmi-di-Programmazione/es5.rkt
@@ -1,4 +1,6 @@
-; Scrivere una funzione "conta-pari-e-dispari" che presa una lista di numeri naturali
-; non negativi ritorna una lista di due elementi che rappresentano rispettivamente
-; il numeri di numeri pari e il numero di dispari nella lista in ingresso.
-; Ad esempio, (conta-pari-e-dispari '(3 5 6 7 9 21 44 657 9)) viene valutata a '(2 7).
+; Scrivere una funzione "conta-occorrenze-ordinate" che presa una lista di numeri naturali
+; non negativi ritorna una lista dove l'elemento in posizione 2k è
+; pari al numeri di occorrenze dell'elemento, nella lista di partenza,
+; in posizione 2k+1. Inoltre, gli elementi in posizione 2k+1 sono ordinati in senso crescente.
+; Ad esempio, (conta-occorrenze-ordinate '(1 2 4 3 2 4 4)) viene valutato a '(1 1 2 2 1 3 3 4).
+; Ad esempio, (conta-occorrenze-ordinate '(3 7 4 3 4 3 1 7 2 1 2 2 7)) viene valutato a '(2 1 3 2 3 3 2 4 3 7).
