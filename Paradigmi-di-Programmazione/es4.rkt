1ef98d38c5ebd1b728b5aff986be940d4ad6b34b
HerrAugust <HerrAugust@users.noreply.github.com>
2015-11-07 21:31:51 +0100

    Create es4.rkt

---
+++ Paradigmi-di-Programmazione/es4.rkt
@@ -0,0 +1,2 @@
+; Scrivere una funzione "conta-pari-e-dispari" che presa una lista di numeri naturali non negativi ritorna una lista di due elementi che rappresentano rispettivamente
+; il numeri di numeri pari e il numero di dispari nella lista in ingresso. Ad esempio, (conta-pari-e-dispari '(3 5 6 7 9 21 44 657 9)) viene valutata a '(2 7).
